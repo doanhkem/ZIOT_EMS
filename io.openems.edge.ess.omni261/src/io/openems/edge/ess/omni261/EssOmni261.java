a1917accfafbf48974aa2d9e62b04bf0d289bee8
Doanh Nguyen <doanhnguyen2.com@gmail.com>
2026-06-16 14:54:43 +0700

    Add OMNI 261 ESS Modbus device

---
+++ io.openems.edge.ess.omni261/src/io/openems/edge/ess/omni261/EssOmni261.java
@@ -0,0 +1,108 @@
+package io.openems.edge.ess.omni261;
+
+import static io.openems.common.channel.AccessMode.READ_WRITE;
+import static io.openems.common.channel.Unit.DEGREE_CELSIUS;
+import static io.openems.common.channel.Unit.DEZIDEGREE_CELSIUS;
+import static io.openems.common.channel.Unit.KILOWATT_HOURS;
+import static io.openems.common.channel.Unit.MILLIAMPERE;
+import static io.openems.common.channel.Unit.MILLIHERTZ;
+import static io.openems.common.channel.Unit.MILLIVOLT;
+import static io.openems.common.channel.Unit.PERCENT;
+import static io.openems.common.channel.Unit.THOUSANDTH;
+import static io.openems.common.channel.Unit.VOLT_AMPERE;
+import static io.openems.common.channel.Unit.VOLT_AMPERE_REACTIVE;
+import static io.openems.common.channel.Unit.WATT;
+import static io.openems.common.types.OpenemsType.INTEGER;
+import static io.openems.common.types.OpenemsType.LONG;
+
+import io.openems.edge.bridge.modbus.api.ModbusComponent;
+import io.openems.edge.common.channel.Doc;
+import io.openems.edge.common.component.OpenemsComponent;
+import io.openems.edge.common.modbusslave.ModbusSlave;
+import io.openems.edge.ess.api.ManagedSymmetricEss;
+import io.openems.edge.ess.api.SymmetricEss;
+
+public interface EssOmni261 extends ManagedSymmetricEss, SymmetricEss, ModbusComponent, OpenemsComponent, ModbusSlave {
+
+	public enum ChannelId implements io.openems.edge.common.channel.ChannelId {
+		SYSTEM_OUTPUT_POWER(Doc.of(INTEGER).unit(WATT)), //
+		SYSTEM_OPERATION_STATUS(Doc.of(INTEGER)), //
+		SYSTEM_SOC(Doc.of(INTEGER).unit(PERCENT)), //
+		BESS_CHARGE_ENERGY_TODAY(Doc.of(LONG).unit(KILOWATT_HOURS)), //
+		BESS_DISCHARGE_ENERGY_TODAY(Doc.of(LONG).unit(KILOWATT_HOURS)), //
+
+		PCS_OPERATION_STATUS(Doc.of(INTEGER)), //
+		PCS_FAULT_STATUS(Doc.of(INTEGER)), //
+		PCS_ACTIVE_POWER(Doc.of(INTEGER).unit(WATT)), //
+		PCS_REACTIVE_POWER(Doc.of(INTEGER).unit(VOLT_AMPERE_REACTIVE)), //
+		PCS_APPARENT_POWER(Doc.of(INTEGER).unit(VOLT_AMPERE)), //
+		PCS_GRID_STATUS(Doc.of(INTEGER)), //
+		PCS_AC_VOLTAGE_L1(Doc.of(INTEGER).unit(MILLIVOLT)), //
+		PCS_AC_VOLTAGE_L2(Doc.of(INTEGER).unit(MILLIVOLT)), //
+		PCS_AC_VOLTAGE_L3(Doc.of(INTEGER).unit(MILLIVOLT)), //
+		PCS_AC_CURRENT_L1(Doc.of(INTEGER).unit(MILLIAMPERE)), //
+		PCS_AC_CURRENT_L2(Doc.of(INTEGER).unit(MILLIAMPERE)), //
+		PCS_AC_CURRENT_L3(Doc.of(INTEGER).unit(MILLIAMPERE)), //
+		PCS_AC_ACTIVE_POWER(Doc.of(INTEGER).unit(WATT)), //
+		PCS_AC_REACTIVE_POWER(Doc.of(INTEGER).unit(VOLT_AMPERE_REACTIVE)), //
+		PCS_AC_APPARENT_POWER(Doc.of(INTEGER).unit(VOLT_AMPERE)), //
+		PCS_AC_FREQUENCY(Doc.of(INTEGER).unit(MILLIHERTZ)), //
+		PCS_POWER_FACTOR(Doc.of(INTEGER).unit(THOUSANDTH)), //
+		PCS_DC_VOLTAGE(Doc.of(INTEGER).unit(MILLIVOLT)), //
+		PCS_DC_CURRENT(Doc.of(INTEGER).unit(MILLIAMPERE)), //
+		PCS_DC_POWER(Doc.of(INTEGER).unit(WATT)), //
+		PCS_TEMPERATURE_1(Doc.of(INTEGER).unit(DEZIDEGREE_CELSIUS)), //
+		PCS_TEMPERATURE_2(Doc.of(INTEGER).unit(DEZIDEGREE_CELSIUS)), //
+		PCS_TEMPERATURE_3(Doc.of(INTEGER).unit(DEZIDEGREE_CELSIUS)), //
+		PCS_TEMPERATURE_4(Doc.of(INTEGER).unit(DEZIDEGREE_CELSIUS)), //
+		PCS_TEMPERATURE_5(Doc.of(INTEGER).unit(DEZIDEGREE_CELSIUS)), //
+		PCS_TEMPERATURE_6(Doc.of(INTEGER).unit(DEZIDEGREE_CELSIUS)), //
+		PCS_TEMPERATURE_7(Doc.of(INTEGER).unit(DEZIDEGREE_CELSIUS)), //
+		PCS_TEMPERATURE_8(Doc.of(INTEGER).unit(DEZIDEGREE_CELSIUS)), //
+		PCS_ACTIVE_POWER_L1(Doc.of(INTEGER).unit(WATT)), //
+		PCS_ACTIVE_POWER_L2(Doc.of(INTEGER).unit(WATT)), //
+		PCS_ACTIVE_POWER_L3(Doc.of(INTEGER).unit(WATT)), //
+		PCS_FAULT_WORD_1(Doc.of(INTEGER)), //
+		PCS_FAULT_WORD_2(Doc.of(INTEGER)), //
+		PCS_FAULT_WORD_3(Doc.of(INTEGER)), //
+		PCS_FAULT_WORD_4(Doc.of(INTEGER)), //
+
+		BESS_DC_VOLTAGE(Doc.of(INTEGER).unit(MILLIVOLT)), //
+		BESS_DC_CURRENT(Doc.of(INTEGER).unit(MILLIAMPERE)), //
+		BESS_SOC(Doc.of(INTEGER).unit(PERCENT)), //
+		BESS_SOH(Doc.of(INTEGER).unit(PERCENT)), //
+		BMS_REQUEST_MAX_CHARGE_CURRENT(Doc.of(INTEGER).unit(MILLIAMPERE)), //
+		BMS_REQUEST_MAX_DISCHARGE_CURRENT(Doc.of(INTEGER).unit(MILLIAMPERE)), //
+		BMS_MAX_CELL_VOLTAGE(Doc.of(INTEGER).unit(MILLIVOLT)), //
+		BMS_MIN_CELL_VOLTAGE(Doc.of(INTEGER).unit(MILLIVOLT)), //
+		BMS_MAX_CELL_VOLTAGE_NUMBER(Doc.of(INTEGER)), //
+		BMS_MIN_CELL_VOLTAGE_NUMBER(Doc.of(INTEGER)), //
+		BMS_MAX_CELL_TEMPERATURE(Doc.of(INTEGER).unit(DEGREE_CELSIUS)), //
+		BMS_MIN_CELL_TEMPERATURE(Doc.of(INTEGER).unit(DEGREE_CELSIUS)), //
+		BMS_MAX_CELL_TEMPERATURE_NUMBER(Doc.of(INTEGER)), //
+		BMS_MIN_CELL_TEMPERATURE_NUMBER(Doc.of(INTEGER)), //
+		BMS_CELL_TEMPERATURE_DELTA(Doc.of(INTEGER).unit(DEGREE_CELSIUS)), //
+		BMS_CELL_VOLTAGE_DELTA(Doc.of(INTEGER).unit(MILLIVOLT)), //
+		BMS_FAULT_WORD(Doc.of(INTEGER)), //
+		BMS_WARNING_WORD(Doc.of(INTEGER)), //
+		BMS_PROTECTED_WORD(Doc.of(INTEGER)), //
+		BMS_SYSTEM_STATUS_HIGH(Doc.of(INTEGER)), //
+		BMS_SYSTEM_STATUS_LOW(Doc.of(INTEGER)), //
+
+		SYSTEM_SHUTDOWN(Doc.of(INTEGER).accessMode(READ_WRITE)), //
+		GRID_MODE_SWITCH(Doc.of(INTEGER).accessMode(READ_WRITE)), //
+		WORKING_MODE(Doc.of(INTEGER).accessMode(READ_WRITE)), //
+		REMOTE_ACTIVE_POWER_SETPOINT(Doc.of(INTEGER).unit(WATT).accessMode(READ_WRITE)); //
+
+		private final Doc doc;
+
+		private ChannelId(Doc doc) {
+			this.doc = doc;
+		}
+
+		@Override
+		public Doc doc() {
+			return this.doc;
+		}
+	}
+}
---
+++ io.openems.edge.ess.omni261/src/io/openems/edge/ess/omni261/EssOmni261Impl.java
@@ -0,0 +1,284 @@
+package io.openems.edge.ess.omni261;
+
+import static io.openems.edge.bridge.modbus.api.ElementToChannelConverter.DIRECT_1_TO_1;
+import static io.openems.edge.bridge.modbus.api.ElementToChannelConverter.MULTIPLY;
+
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventHandler;
+import org.osgi.service.event.propertytypes.EventTopics;
+import org.osgi.service.metatype.annotations.Designate;
+
+import io.openems.common.channel.AccessMode;
+import io.openems.common.exceptions.OpenemsError.OpenemsNamedException;
+import io.openems.common.exceptions.OpenemsException;
+import io.openems.edge.bridge.modbus.api.AbstractOpenemsModbusComponent;
+import io.openems.edge.bridge.modbus.api.BridgeModbus;
+import io.openems.edge.bridge.modbus.api.ModbusComponent;
+import io.openems.edge.bridge.modbus.api.ModbusProtocol;
+import io.openems.edge.bridge.modbus.api.element.SignedWordElement;
+import io.openems.edge.bridge.modbus.api.element.UnsignedDoublewordElement;
+import io.openems.edge.bridge.modbus.api.element.UnsignedWordElement;
+import io.openems.edge.bridge.modbus.api.task.FC3ReadRegistersTask;
+import io.openems.edge.bridge.modbus.api.task.FC6WriteRegisterTask;
+import io.openems.edge.common.channel.IntegerWriteChannel;
+import io.openems.edge.common.component.OpenemsComponent;
+import io.openems.edge.common.event.EdgeEventConstants;
+import io.openems.edge.common.modbusslave.ModbusSlave;
+import io.openems.edge.common.modbusslave.ModbusSlaveTable;
+import io.openems.edge.common.sum.GridMode;
+import io.openems.edge.common.taskmanager.Priority;
+import io.openems.edge.ess.api.ManagedSymmetricEss;
+import io.openems.edge.ess.api.SymmetricEss;
+import io.openems.edge.ess.power.api.Power;
+import io.openems.edge.timedata.api.Timedata;
+import io.openems.edge.timedata.api.TimedataProvider;
+
+@Designate(ocd = Config.class, factory = true)
+@Component(//
+		name = "Ess.Omni261", //
+		immediate = true, //
+		configurationPolicy = ConfigurationPolicy.REQUIRE //
+)
+@EventTopics({ //
+		EdgeEventConstants.TOPIC_CYCLE_AFTER_PROCESS_IMAGE, //
+})
+public class EssOmni261Impl extends AbstractOpenemsModbusComponent
+		implements EssOmni261, ManagedSymmetricEss, SymmetricEss, ModbusComponent, OpenemsComponent, EventHandler,
+		ModbusSlave, TimedataProvider {
+
+	@Reference
+	private ConfigurationAdmin cm;
+
+	@Reference
+	private Power power;
+
+	@Reference(policy = ReferencePolicy.DYNAMIC, policyOption = ReferencePolicy.GREEDY, cardinality = ReferenceCardinality.OPTIONAL)
+	private volatile Timedata timedata = null;
+
+	private Config config;
+
+	public EssOmni261Impl() {
+		super(//
+				OpenemsComponent.ChannelId.values(), //
+				ModbusComponent.ChannelId.values(), //
+				SymmetricEss.ChannelId.values(), //
+				ManagedSymmetricEss.ChannelId.values(), //
+				EssOmni261.ChannelId.values() //
+		);
+	}
+
+	@Override
+	@Reference(policy = ReferencePolicy.STATIC, policyOption = ReferencePolicyOption.GREEDY, cardinality = ReferenceCardinality.MANDATORY)
+	protected void setModbus(BridgeModbus modbus) {
+		super.setModbus(modbus);
+	}
+
+	@Activate
+	private void activate(ComponentContext context, Config config) throws OpenemsException {
+		this.config = config;
+
+		if (super.activate(context, config.id(), config.alias(), config.enabled(), config.modbusUnitId(), this.cm,
+				"Modbus", config.modbus_id())) {
+			return;
+		}
+
+		this.updateConfiguredLimits();
+	}
+
+	@Override
+	@Deactivate
+	protected void deactivate() {
+		super.deactivate();
+	}
+
+	@Override
+	protected ModbusProtocol defineModbusProtocol() {
+		return new ModbusProtocol(this, //
+				new FC3ReadRegistersTask(570, Priority.LOW, //
+						m(ChannelId.SYSTEM_OUTPUT_POWER, new SignedWordElement(570), MULTIPLY(100))), //
+				new FC3ReadRegistersTask(587, Priority.LOW, //
+						m(ChannelId.SYSTEM_SOC, new UnsignedWordElement(587), MULTIPLY(0.1)), //
+						m(ChannelId.BESS_CHARGE_ENERGY_TODAY, new UnsignedDoublewordElement(588)), //
+						m(ChannelId.BESS_DISCHARGE_ENERGY_TODAY, new UnsignedDoublewordElement(590))), //
+				new FC3ReadRegistersTask(952, Priority.LOW, //
+						m(ChannelId.SYSTEM_OPERATION_STATUS, new UnsignedWordElement(952))), //
+
+				new FC3ReadRegistersTask(25101, Priority.HIGH, //
+						m(ChannelId.PCS_OPERATION_STATUS, new UnsignedWordElement(25101)), //
+						m(ChannelId.PCS_FAULT_STATUS, new UnsignedWordElement(25102)), //
+						m(new SignedWordElement(25103)) //
+								.m(ChannelId.PCS_ACTIVE_POWER, MULTIPLY(100)) //
+								.m(SymmetricEss.ChannelId.ACTIVE_POWER, MULTIPLY(100)) //
+								.build(), //
+						m(new SignedWordElement(25104)) //
+								.m(ChannelId.PCS_REACTIVE_POWER, MULTIPLY(100)) //
+								.m(SymmetricEss.ChannelId.REACTIVE_POWER, MULTIPLY(100)) //
+								.build(), //
+						m(ChannelId.PCS_APPARENT_POWER, new SignedWordElement(25105), MULTIPLY(100)), //
+						m(ChannelId.PCS_GRID_STATUS, new UnsignedWordElement(25106)).build()
+								.onUpdateCallback(this::updateGridMode), //
+						m(ChannelId.PCS_AC_VOLTAGE_L1, new UnsignedWordElement(25107), MULTIPLY(100)), //
+						m(ChannelId.PCS_AC_VOLTAGE_L2, new UnsignedWordElement(25108), MULTIPLY(100)), //
+						m(ChannelId.PCS_AC_VOLTAGE_L3, new UnsignedWordElement(25109), MULTIPLY(100)), //
+						m(ChannelId.PCS_AC_CURRENT_L1, new SignedWordElement(25110), MULTIPLY(100)), //
+						m(ChannelId.PCS_AC_CURRENT_L2, new SignedWordElement(25111), MULTIPLY(100)), //
+						m(ChannelId.PCS_AC_CURRENT_L3, new SignedWordElement(25112), MULTIPLY(100)), //
+						m(ChannelId.PCS_AC_ACTIVE_POWER, new SignedWordElement(25113), MULTIPLY(100)), //
+						m(ChannelId.PCS_AC_REACTIVE_POWER, new SignedWordElement(25114), MULTIPLY(100)), //
+						m(ChannelId.PCS_AC_APPARENT_POWER, new SignedWordElement(25115), MULTIPLY(100)), //
+						m(ChannelId.PCS_AC_FREQUENCY, new UnsignedWordElement(25116), MULTIPLY(10)), //
+						m(ChannelId.PCS_POWER_FACTOR, new SignedWordElement(25117)), //
+						m(ChannelId.PCS_DC_VOLTAGE, new UnsignedWordElement(25118), MULTIPLY(100)), //
+						m(ChannelId.PCS_DC_CURRENT, new SignedWordElement(25119), MULTIPLY(100)), //
+						m(ChannelId.PCS_DC_POWER, new SignedWordElement(25120), MULTIPLY(100)), //
+						m(ChannelId.PCS_TEMPERATURE_1, new SignedWordElement(25121)), //
+						m(ChannelId.PCS_TEMPERATURE_2, new SignedWordElement(25122)), //
+						m(ChannelId.PCS_TEMPERATURE_3, new SignedWordElement(25123)), //
+						m(ChannelId.PCS_TEMPERATURE_4, new SignedWordElement(25124)), //
+						m(ChannelId.PCS_TEMPERATURE_5, new SignedWordElement(25125)), //
+						m(ChannelId.PCS_TEMPERATURE_6, new SignedWordElement(25126)), //
+						m(ChannelId.PCS_TEMPERATURE_7, new SignedWordElement(25127)), //
+						m(ChannelId.PCS_TEMPERATURE_8, new SignedWordElement(25128)), //
+						m(ChannelId.PCS_ACTIVE_POWER_L1, new SignedWordElement(25129), MULTIPLY(100)), //
+						m(ChannelId.PCS_ACTIVE_POWER_L2, new SignedWordElement(25130), MULTIPLY(100)), //
+						m(ChannelId.PCS_ACTIVE_POWER_L3, new SignedWordElement(25131), MULTIPLY(100)), //
+						m(ChannelId.PCS_FAULT_WORD_1, new UnsignedWordElement(25132)), //
+						m(ChannelId.PCS_FAULT_WORD_2, new UnsignedWordElement(25133)), //
+						m(ChannelId.PCS_FAULT_WORD_3, new UnsignedWordElement(25134)), //
+						m(ChannelId.PCS_FAULT_WORD_4, new UnsignedWordElement(25135))), //
+
+				new FC3ReadRegistersTask(25200, Priority.HIGH, //
+						m(ChannelId.BESS_DC_VOLTAGE, new SignedWordElement(25200), MULTIPLY(100)), //
+						m(ChannelId.BESS_DC_CURRENT, new SignedWordElement(25201), MULTIPLY(1000)), //
+						m(new SignedWordElement(25202)) //
+								.m(ChannelId.BESS_SOC, DIRECT_1_TO_1) //
+								.m(SymmetricEss.ChannelId.SOC, DIRECT_1_TO_1) //
+								.build(), //
+						m(ChannelId.BESS_SOH, new SignedWordElement(25203)), //
+						m(ChannelId.BMS_REQUEST_MAX_CHARGE_CURRENT, new SignedWordElement(25204), MULTIPLY(1000)), //
+						m(ChannelId.BMS_REQUEST_MAX_DISCHARGE_CURRENT, new SignedWordElement(25205), MULTIPLY(1000)), //
+						m(new SignedWordElement(25206)) //
+								.m(ChannelId.BMS_MAX_CELL_VOLTAGE, DIRECT_1_TO_1) //
+								.m(SymmetricEss.ChannelId.MAX_CELL_VOLTAGE, DIRECT_1_TO_1) //
+								.build(), //
+						m(new SignedWordElement(25207)) //
+								.m(ChannelId.BMS_MIN_CELL_VOLTAGE, DIRECT_1_TO_1) //
+								.m(SymmetricEss.ChannelId.MIN_CELL_VOLTAGE, DIRECT_1_TO_1) //
+								.build(), //
+						m(ChannelId.BMS_MAX_CELL_VOLTAGE_NUMBER, new SignedWordElement(25208)), //
+						m(ChannelId.BMS_MIN_CELL_VOLTAGE_NUMBER, new SignedWordElement(25209)), //
+						m(new SignedWordElement(25210)) //
+								.m(ChannelId.BMS_MAX_CELL_TEMPERATURE, DIRECT_1_TO_1) //
+								.m(SymmetricEss.ChannelId.MAX_CELL_TEMPERATURE, DIRECT_1_TO_1) //
+								.build(), //
+						m(new SignedWordElement(25211)) //
+								.m(ChannelId.BMS_MIN_CELL_TEMPERATURE, DIRECT_1_TO_1) //
+								.m(SymmetricEss.ChannelId.MIN_CELL_TEMPERATURE, DIRECT_1_TO_1) //
+								.build(), //
+						m(ChannelId.BMS_MAX_CELL_TEMPERATURE_NUMBER, new SignedWordElement(25212)), //
+						m(ChannelId.BMS_MIN_CELL_TEMPERATURE_NUMBER, new SignedWordElement(25213)), //
+						m(ChannelId.BMS_CELL_TEMPERATURE_DELTA, new SignedWordElement(25214)), //
+						m(ChannelId.BMS_CELL_VOLTAGE_DELTA, new SignedWordElement(25215)), //
+						m(ChannelId.BMS_FAULT_WORD, new UnsignedWordElement(25216)), //
+						m(ChannelId.BMS_WARNING_WORD, new UnsignedWordElement(25217)), //
+						m(ChannelId.BMS_PROTECTED_WORD, new UnsignedWordElement(25218)), //
+						m(ChannelId.BMS_SYSTEM_STATUS_HIGH, new UnsignedWordElement(25219)), //
+						m(ChannelId.BMS_SYSTEM_STATUS_LOW, new UnsignedWordElement(25220))), //
+
+				new FC3ReadRegistersTask(611, Priority.LOW, //
+						m(ChannelId.SYSTEM_SHUTDOWN, new SignedWordElement(611)), //
+						m(ChannelId.GRID_MODE_SWITCH, new SignedWordElement(612))), //
+				new FC3ReadRegistersTask(615, Priority.LOW, //
+						m(ChannelId.WORKING_MODE, new UnsignedWordElement(615))), //
+				new FC3ReadRegistersTask(618, Priority.LOW, //
+						m(ChannelId.REMOTE_ACTIVE_POWER_SETPOINT, new SignedWordElement(618), MULTIPLY(100))), //
+
+				new FC6WriteRegisterTask(611, m(ChannelId.SYSTEM_SHUTDOWN, new SignedWordElement(611))), //
+				new FC6WriteRegisterTask(612, m(ChannelId.GRID_MODE_SWITCH, new SignedWordElement(612))), //
+				new FC6WriteRegisterTask(615, m(ChannelId.WORKING_MODE, new UnsignedWordElement(615))), //
+				new FC6WriteRegisterTask(618,
+						m(ChannelId.REMOTE_ACTIVE_POWER_SETPOINT, new SignedWordElement(618), MULTIPLY(100))) //
+		);
+	}
+
+	@Override
+	public void handleEvent(Event event) {
+		if (!this.isEnabled()) {
+			return;
+		}
+		switch (event.getTopic()) {
+		case EdgeEventConstants.TOPIC_CYCLE_AFTER_PROCESS_IMAGE -> this.updateConfiguredLimits();
+		}
+	}
+
+	@Override
+	public Power getPower() {
+		return this.power;
+	}
+
+	@Override
+	public void applyPower(int activePower, int reactivePower) throws OpenemsNamedException {
+		if (this.config.readOnly()) {
+			return;
+		}
+		this.<IntegerWriteChannel>channel(ChannelId.WORKING_MODE).setNextWriteValue(2);
+		this.<IntegerWriteChannel>channel(ChannelId.REMOTE_ACTIVE_POWER_SETPOINT).setNextWriteValue(activePower);
+	}
+
+	@Override
+	public int getPowerPrecision() {
+		return 100;
+	}
+
+	@Override
+	public Timedata getTimedata() {
+		return this.timedata;
+	}
+
+	@Override
+	public String debugLog() {
+		return "SoC:" + this.getSoc().asString() + "|L:" + this.getActivePower().asString();
+	}
+
+	@Override
+	public ModbusSlaveTable getModbusSlaveTable(AccessMode accessMode) {
+		return new ModbusSlaveTable(//
+				OpenemsComponent.getModbusSlaveNatureTable(accessMode), //
+				SymmetricEss.getModbusSlaveNatureTable(accessMode), //
+				ManagedSymmetricEss.getModbusSlaveNatureTable(accessMode) //
+		);
+	}
+
+	private void updateConfiguredLimits() {
+		if (this.config == null) {
+			return;
+		}
+		this._setCapacity(this.config.capacity());
+		this._setMaxApparentPower(this.config.maxApparentPower());
+		this._setAllowedChargePower(-Math.abs(this.config.maxChargePower()));
+		this._setAllowedDischargePower(Math.abs(this.config.maxDischargePower()));
+	}
+
+	private void updateGridMode(Object value) {
+		if (!(value instanceof Number number)) {
+			this.getGridModeChannel().setNextValue(GridMode.UNDEFINED);
+			return;
+		}
+		switch (number.intValue()) {
+		case 3 -> this.getGridModeChannel().setNextValue(GridMode.ON_GRID);
+		case 4 -> this.getGridModeChannel().setNextValue(GridMode.OFF_GRID);
+		default -> this.getGridModeChannel().setNextValue(GridMode.UNDEFINED);
+		}
+	}
+
+}
