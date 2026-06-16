1b6919311edae7952a635e99f08f2c5b30cf6a51
Doanh Nguyen <doanhnguyen2.com@gmail.com>
2026-06-16 15:17:00 +0700

    Fix OMNI 261 ESS compile errors

--- io.openems.edge.ess.omni261/src/io/openems/edge/ess/omni261/EssOmni261Impl.java
+++ io.openems.edge.ess.omni261/src/io/openems/edge/ess/omni261/EssOmni261Impl.java
@@ -62,7 +62,11 @@ public class EssOmni261Impl extends AbstractOpenemsModbusComponent
 	@Reference
 	private Power power;
 
-	@Reference(policy = ReferencePolicy.DYNAMIC, policyOption = ReferencePolicy.GREEDY, cardinality = ReferenceCardinality.OPTIONAL)
+	@Reference(//
+			policy = ReferencePolicy.DYNAMIC, //
+			policyOption = ReferencePolicyOption.GREEDY, //
+			cardinality = ReferenceCardinality.OPTIONAL //
+	)
 	private volatile Timedata timedata = null;
 
 	private Config config;
@@ -78,7 +82,11 @@ public class EssOmni261Impl extends AbstractOpenemsModbusComponent
 	}
 
 	@Override
-	@Reference(policy = ReferencePolicy.STATIC, policyOption = ReferencePolicyOption.GREEDY, cardinality = ReferenceCardinality.MANDATORY)
+	@Reference(//
+			policy = ReferencePolicy.STATIC, //
+			policyOption = ReferencePolicyOption.GREEDY, //
+			cardinality = ReferenceCardinality.MANDATORY //
+	)
 	protected void setModbus(BridgeModbus modbus) {
 		super.setModbus(modbus);
 	}
@@ -105,109 +113,111 @@ public class EssOmni261Impl extends AbstractOpenemsModbusComponent
 	protected ModbusProtocol defineModbusProtocol() {
 		return new ModbusProtocol(this, //
 				new FC3ReadRegistersTask(570, Priority.LOW, //
-						m(ChannelId.SYSTEM_OUTPUT_POWER, new SignedWordElement(570), MULTIPLY(100))), //
+						m(EssOmni261.ChannelId.SYSTEM_OUTPUT_POWER, new SignedWordElement(570), MULTIPLY(100))), //
 				new FC3ReadRegistersTask(587, Priority.LOW, //
-						m(ChannelId.SYSTEM_SOC, new UnsignedWordElement(587), MULTIPLY(0.1)), //
-						m(ChannelId.BESS_CHARGE_ENERGY_TODAY, new UnsignedDoublewordElement(588)), //
-						m(ChannelId.BESS_DISCHARGE_ENERGY_TODAY, new UnsignedDoublewordElement(590))), //
+						m(EssOmni261.ChannelId.SYSTEM_SOC, new UnsignedWordElement(587), MULTIPLY(0.1)), //
+						m(EssOmni261.ChannelId.BESS_CHARGE_ENERGY_TODAY, new UnsignedDoublewordElement(588)), //
+						m(EssOmni261.ChannelId.BESS_DISCHARGE_ENERGY_TODAY, new UnsignedDoublewordElement(590))), //
 				new FC3ReadRegistersTask(952, Priority.LOW, //
-						m(ChannelId.SYSTEM_OPERATION_STATUS, new UnsignedWordElement(952))), //
+						m(EssOmni261.ChannelId.SYSTEM_OPERATION_STATUS, new UnsignedWordElement(952))), //
 
 				new FC3ReadRegistersTask(25101, Priority.HIGH, //
-						m(ChannelId.PCS_OPERATION_STATUS, new UnsignedWordElement(25101)), //
-						m(ChannelId.PCS_FAULT_STATUS, new UnsignedWordElement(25102)), //
+						m(EssOmni261.ChannelId.PCS_OPERATION_STATUS, new UnsignedWordElement(25101)), //
+						m(EssOmni261.ChannelId.PCS_FAULT_STATUS, new UnsignedWordElement(25102)), //
 						m(new SignedWordElement(25103)) //
-								.m(ChannelId.PCS_ACTIVE_POWER, MULTIPLY(100)) //
+								.m(EssOmni261.ChannelId.PCS_ACTIVE_POWER, MULTIPLY(100)) //
 								.m(SymmetricEss.ChannelId.ACTIVE_POWER, MULTIPLY(100)) //
 								.build(), //
 						m(new SignedWordElement(25104)) //
-								.m(ChannelId.PCS_REACTIVE_POWER, MULTIPLY(100)) //
+								.m(EssOmni261.ChannelId.PCS_REACTIVE_POWER, MULTIPLY(100)) //
 								.m(SymmetricEss.ChannelId.REACTIVE_POWER, MULTIPLY(100)) //
 								.build(), //
-						m(ChannelId.PCS_APPARENT_POWER, new SignedWordElement(25105), MULTIPLY(100)), //
-						m(ChannelId.PCS_GRID_STATUS, new UnsignedWordElement(25106)).build()
+						m(EssOmni261.ChannelId.PCS_APPARENT_POWER, new SignedWordElement(25105), MULTIPLY(100)), //
+						m(new UnsignedWordElement(25106)) //
+								.m(EssOmni261.ChannelId.PCS_GRID_STATUS, DIRECT_1_TO_1) //
+								.build()
 								.onUpdateCallback(this::updateGridMode), //
-						m(ChannelId.PCS_AC_VOLTAGE_L1, new UnsignedWordElement(25107), MULTIPLY(100)), //
-						m(ChannelId.PCS_AC_VOLTAGE_L2, new UnsignedWordElement(25108), MULTIPLY(100)), //
-						m(ChannelId.PCS_AC_VOLTAGE_L3, new UnsignedWordElement(25109), MULTIPLY(100)), //
-						m(ChannelId.PCS_AC_CURRENT_L1, new SignedWordElement(25110), MULTIPLY(100)), //
-						m(ChannelId.PCS_AC_CURRENT_L2, new SignedWordElement(25111), MULTIPLY(100)), //
-						m(ChannelId.PCS_AC_CURRENT_L3, new SignedWordElement(25112), MULTIPLY(100)), //
-						m(ChannelId.PCS_AC_ACTIVE_POWER, new SignedWordElement(25113), MULTIPLY(100)), //
-						m(ChannelId.PCS_AC_REACTIVE_POWER, new SignedWordElement(25114), MULTIPLY(100)), //
-						m(ChannelId.PCS_AC_APPARENT_POWER, new SignedWordElement(25115), MULTIPLY(100)), //
-						m(ChannelId.PCS_AC_FREQUENCY, new UnsignedWordElement(25116), MULTIPLY(10)), //
-						m(ChannelId.PCS_POWER_FACTOR, new SignedWordElement(25117)), //
-						m(ChannelId.PCS_DC_VOLTAGE, new UnsignedWordElement(25118), MULTIPLY(100)), //
-						m(ChannelId.PCS_DC_CURRENT, new SignedWordElement(25119), MULTIPLY(100)), //
-						m(ChannelId.PCS_DC_POWER, new SignedWordElement(25120), MULTIPLY(100)), //
-						m(ChannelId.PCS_TEMPERATURE_1, new SignedWordElement(25121)), //
-						m(ChannelId.PCS_TEMPERATURE_2, new SignedWordElement(25122)), //
-						m(ChannelId.PCS_TEMPERATURE_3, new SignedWordElement(25123)), //
-						m(ChannelId.PCS_TEMPERATURE_4, new SignedWordElement(25124)), //
-						m(ChannelId.PCS_TEMPERATURE_5, new SignedWordElement(25125)), //
-						m(ChannelId.PCS_TEMPERATURE_6, new SignedWordElement(25126)), //
-						m(ChannelId.PCS_TEMPERATURE_7, new SignedWordElement(25127)), //
-						m(ChannelId.PCS_TEMPERATURE_8, new SignedWordElement(25128)), //
-						m(ChannelId.PCS_ACTIVE_POWER_L1, new SignedWordElement(25129), MULTIPLY(100)), //
-						m(ChannelId.PCS_ACTIVE_POWER_L2, new SignedWordElement(25130), MULTIPLY(100)), //
-						m(ChannelId.PCS_ACTIVE_POWER_L3, new SignedWordElement(25131), MULTIPLY(100)), //
-						m(ChannelId.PCS_FAULT_WORD_1, new UnsignedWordElement(25132)), //
-						m(ChannelId.PCS_FAULT_WORD_2, new UnsignedWordElement(25133)), //
-						m(ChannelId.PCS_FAULT_WORD_3, new UnsignedWordElement(25134)), //
-						m(ChannelId.PCS_FAULT_WORD_4, new UnsignedWordElement(25135))), //
+						m(EssOmni261.ChannelId.PCS_AC_VOLTAGE_L1, new UnsignedWordElement(25107), MULTIPLY(100)), //
+						m(EssOmni261.ChannelId.PCS_AC_VOLTAGE_L2, new UnsignedWordElement(25108), MULTIPLY(100)), //
+						m(EssOmni261.ChannelId.PCS_AC_VOLTAGE_L3, new UnsignedWordElement(25109), MULTIPLY(100)), //
+						m(EssOmni261.ChannelId.PCS_AC_CURRENT_L1, new SignedWordElement(25110), MULTIPLY(100)), //
+						m(EssOmni261.ChannelId.PCS_AC_CURRENT_L2, new SignedWordElement(25111), MULTIPLY(100)), //
+						m(EssOmni261.ChannelId.PCS_AC_CURRENT_L3, new SignedWordElement(25112), MULTIPLY(100)), //
+						m(EssOmni261.ChannelId.PCS_AC_ACTIVE_POWER, new SignedWordElement(25113), MULTIPLY(100)), //
+						m(EssOmni261.ChannelId.PCS_AC_REACTIVE_POWER, new SignedWordElement(25114), MULTIPLY(100)), //
+						m(EssOmni261.ChannelId.PCS_AC_APPARENT_POWER, new SignedWordElement(25115), MULTIPLY(100)), //
+						m(EssOmni261.ChannelId.PCS_AC_FREQUENCY, new UnsignedWordElement(25116), MULTIPLY(10)), //
+						m(EssOmni261.ChannelId.PCS_POWER_FACTOR, new SignedWordElement(25117)), //
+						m(EssOmni261.ChannelId.PCS_DC_VOLTAGE, new UnsignedWordElement(25118), MULTIPLY(100)), //
+						m(EssOmni261.ChannelId.PCS_DC_CURRENT, new SignedWordElement(25119), MULTIPLY(100)), //
+						m(EssOmni261.ChannelId.PCS_DC_POWER, new SignedWordElement(25120), MULTIPLY(100)), //
+						m(EssOmni261.ChannelId.PCS_TEMPERATURE_1, new SignedWordElement(25121)), //
+						m(EssOmni261.ChannelId.PCS_TEMPERATURE_2, new SignedWordElement(25122)), //
+						m(EssOmni261.ChannelId.PCS_TEMPERATURE_3, new SignedWordElement(25123)), //
+						m(EssOmni261.ChannelId.PCS_TEMPERATURE_4, new SignedWordElement(25124)), //
+						m(EssOmni261.ChannelId.PCS_TEMPERATURE_5, new SignedWordElement(25125)), //
+						m(EssOmni261.ChannelId.PCS_TEMPERATURE_6, new SignedWordElement(25126)), //
+						m(EssOmni261.ChannelId.PCS_TEMPERATURE_7, new SignedWordElement(25127)), //
+						m(EssOmni261.ChannelId.PCS_TEMPERATURE_8, new SignedWordElement(25128)), //
+						m(EssOmni261.ChannelId.PCS_ACTIVE_POWER_L1, new SignedWordElement(25129), MULTIPLY(100)), //
+						m(EssOmni261.ChannelId.PCS_ACTIVE_POWER_L2, new SignedWordElement(25130), MULTIPLY(100)), //
+						m(EssOmni261.ChannelId.PCS_ACTIVE_POWER_L3, new SignedWordElement(25131), MULTIPLY(100)), //
+						m(EssOmni261.ChannelId.PCS_FAULT_WORD_1, new UnsignedWordElement(25132)), //
+						m(EssOmni261.ChannelId.PCS_FAULT_WORD_2, new UnsignedWordElement(25133)), //
+						m(EssOmni261.ChannelId.PCS_FAULT_WORD_3, new UnsignedWordElement(25134)), //
+						m(EssOmni261.ChannelId.PCS_FAULT_WORD_4, new UnsignedWordElement(25135))), //
 
 				new FC3ReadRegistersTask(25200, Priority.HIGH, //
-						m(ChannelId.BESS_DC_VOLTAGE, new SignedWordElement(25200), MULTIPLY(100)), //
-						m(ChannelId.BESS_DC_CURRENT, new SignedWordElement(25201), MULTIPLY(1000)), //
+						m(EssOmni261.ChannelId.BESS_DC_VOLTAGE, new SignedWordElement(25200), MULTIPLY(100)), //
+						m(EssOmni261.ChannelId.BESS_DC_CURRENT, new SignedWordElement(25201), MULTIPLY(1000)), //
 						m(new SignedWordElement(25202)) //
-								.m(ChannelId.BESS_SOC, DIRECT_1_TO_1) //
+								.m(EssOmni261.ChannelId.BESS_SOC, DIRECT_1_TO_1) //
 								.m(SymmetricEss.ChannelId.SOC, DIRECT_1_TO_1) //
 								.build(), //
-						m(ChannelId.BESS_SOH, new SignedWordElement(25203)), //
-						m(ChannelId.BMS_REQUEST_MAX_CHARGE_CURRENT, new SignedWordElement(25204), MULTIPLY(1000)), //
-						m(ChannelId.BMS_REQUEST_MAX_DISCHARGE_CURRENT, new SignedWordElement(25205), MULTIPLY(1000)), //
+						m(EssOmni261.ChannelId.BESS_SOH, new SignedWordElement(25203)), //
+						m(EssOmni261.ChannelId.BMS_REQUEST_MAX_CHARGE_CURRENT, new SignedWordElement(25204), MULTIPLY(1000)), //
+						m(EssOmni261.ChannelId.BMS_REQUEST_MAX_DISCHARGE_CURRENT, new SignedWordElement(25205), MULTIPLY(1000)), //
 						m(new SignedWordElement(25206)) //
-								.m(ChannelId.BMS_MAX_CELL_VOLTAGE, DIRECT_1_TO_1) //
+								.m(EssOmni261.ChannelId.BMS_MAX_CELL_VOLTAGE, DIRECT_1_TO_1) //
 								.m(SymmetricEss.ChannelId.MAX_CELL_VOLTAGE, DIRECT_1_TO_1) //
 								.build(), //
 						m(new SignedWordElement(25207)) //
-								.m(ChannelId.BMS_MIN_CELL_VOLTAGE, DIRECT_1_TO_1) //
+								.m(EssOmni261.ChannelId.BMS_MIN_CELL_VOLTAGE, DIRECT_1_TO_1) //
 								.m(SymmetricEss.ChannelId.MIN_CELL_VOLTAGE, DIRECT_1_TO_1) //
 								.build(), //
-						m(ChannelId.BMS_MAX_CELL_VOLTAGE_NUMBER, new SignedWordElement(25208)), //
-						m(ChannelId.BMS_MIN_CELL_VOLTAGE_NUMBER, new SignedWordElement(25209)), //
+						m(EssOmni261.ChannelId.BMS_MAX_CELL_VOLTAGE_NUMBER, new SignedWordElement(25208)), //
+						m(EssOmni261.ChannelId.BMS_MIN_CELL_VOLTAGE_NUMBER, new SignedWordElement(25209)), //
 						m(new SignedWordElement(25210)) //
-								.m(ChannelId.BMS_MAX_CELL_TEMPERATURE, DIRECT_1_TO_1) //
+								.m(EssOmni261.ChannelId.BMS_MAX_CELL_TEMPERATURE, DIRECT_1_TO_1) //
 								.m(SymmetricEss.ChannelId.MAX_CELL_TEMPERATURE, DIRECT_1_TO_1) //
 								.build(), //
 						m(new SignedWordElement(25211)) //
-								.m(ChannelId.BMS_MIN_CELL_TEMPERATURE, DIRECT_1_TO_1) //
+								.m(EssOmni261.ChannelId.BMS_MIN_CELL_TEMPERATURE, DIRECT_1_TO_1) //
 								.m(SymmetricEss.ChannelId.MIN_CELL_TEMPERATURE, DIRECT_1_TO_1) //
 								.build(), //
-						m(ChannelId.BMS_MAX_CELL_TEMPERATURE_NUMBER, new SignedWordElement(25212)), //
-						m(ChannelId.BMS_MIN_CELL_TEMPERATURE_NUMBER, new SignedWordElement(25213)), //
-						m(ChannelId.BMS_CELL_TEMPERATURE_DELTA, new SignedWordElement(25214)), //
-						m(ChannelId.BMS_CELL_VOLTAGE_DELTA, new SignedWordElement(25215)), //
-						m(ChannelId.BMS_FAULT_WORD, new UnsignedWordElement(25216)), //
-						m(ChannelId.BMS_WARNING_WORD, new UnsignedWordElement(25217)), //
-						m(ChannelId.BMS_PROTECTED_WORD, new UnsignedWordElement(25218)), //
-						m(ChannelId.BMS_SYSTEM_STATUS_HIGH, new UnsignedWordElement(25219)), //
-						m(ChannelId.BMS_SYSTEM_STATUS_LOW, new UnsignedWordElement(25220))), //
+						m(EssOmni261.ChannelId.BMS_MAX_CELL_TEMPERATURE_NUMBER, new SignedWordElement(25212)), //
+						m(EssOmni261.ChannelId.BMS_MIN_CELL_TEMPERATURE_NUMBER, new SignedWordElement(25213)), //
+						m(EssOmni261.ChannelId.BMS_CELL_TEMPERATURE_DELTA, new SignedWordElement(25214)), //
+						m(EssOmni261.ChannelId.BMS_CELL_VOLTAGE_DELTA, new SignedWordElement(25215)), //
+						m(EssOmni261.ChannelId.BMS_FAULT_WORD, new UnsignedWordElement(25216)), //
+						m(EssOmni261.ChannelId.BMS_WARNING_WORD, new UnsignedWordElement(25217)), //
+						m(EssOmni261.ChannelId.BMS_PROTECTED_WORD, new UnsignedWordElement(25218)), //
+						m(EssOmni261.ChannelId.BMS_SYSTEM_STATUS_HIGH, new UnsignedWordElement(25219)), //
+						m(EssOmni261.ChannelId.BMS_SYSTEM_STATUS_LOW, new UnsignedWordElement(25220))), //
 
 				new FC3ReadRegistersTask(611, Priority.LOW, //
-						m(ChannelId.SYSTEM_SHUTDOWN, new SignedWordElement(611)), //
-						m(ChannelId.GRID_MODE_SWITCH, new SignedWordElement(612))), //
+						m(EssOmni261.ChannelId.SYSTEM_SHUTDOWN, new SignedWordElement(611)), //
+						m(EssOmni261.ChannelId.GRID_MODE_SWITCH, new SignedWordElement(612))), //
 				new FC3ReadRegistersTask(615, Priority.LOW, //
-						m(ChannelId.WORKING_MODE, new UnsignedWordElement(615))), //
+						m(EssOmni261.ChannelId.WORKING_MODE, new UnsignedWordElement(615))), //
 				new FC3ReadRegistersTask(618, Priority.LOW, //
-						m(ChannelId.REMOTE_ACTIVE_POWER_SETPOINT, new SignedWordElement(618), MULTIPLY(100))), //
+						m(EssOmni261.ChannelId.REMOTE_ACTIVE_POWER_SETPOINT, new SignedWordElement(618), MULTIPLY(100))), //
 
-				new FC6WriteRegisterTask(611, m(ChannelId.SYSTEM_SHUTDOWN, new SignedWordElement(611))), //
-				new FC6WriteRegisterTask(612, m(ChannelId.GRID_MODE_SWITCH, new SignedWordElement(612))), //
-				new FC6WriteRegisterTask(615, m(ChannelId.WORKING_MODE, new UnsignedWordElement(615))), //
+				new FC6WriteRegisterTask(611, m(EssOmni261.ChannelId.SYSTEM_SHUTDOWN, new SignedWordElement(611))), //
+				new FC6WriteRegisterTask(612, m(EssOmni261.ChannelId.GRID_MODE_SWITCH, new SignedWordElement(612))), //
+				new FC6WriteRegisterTask(615, m(EssOmni261.ChannelId.WORKING_MODE, new UnsignedWordElement(615))), //
 				new FC6WriteRegisterTask(618,
-						m(ChannelId.REMOTE_ACTIVE_POWER_SETPOINT, new SignedWordElement(618), MULTIPLY(100))) //
+						m(EssOmni261.ChannelId.REMOTE_ACTIVE_POWER_SETPOINT, new SignedWordElement(618), MULTIPLY(100))) //
 		);
 	}
 
@@ -231,8 +241,8 @@ public class EssOmni261Impl extends AbstractOpenemsModbusComponent
 		if (this.config.readOnly()) {
 			return;
 		}
-		this.<IntegerWriteChannel>channel(ChannelId.WORKING_MODE).setNextWriteValue(2);
-		this.<IntegerWriteChannel>channel(ChannelId.REMOTE_ACTIVE_POWER_SETPOINT).setNextWriteValue(activePower);
+		this.<IntegerWriteChannel>channel(EssOmni261.ChannelId.WORKING_MODE).setNextWriteValue(2);
+		this.<IntegerWriteChannel>channel(EssOmni261.ChannelId.REMOTE_ACTIVE_POWER_SETPOINT).setNextWriteValue(activePower);
 	}
 
 	@Override
@@ -265,7 +275,7 @@ public class EssOmni261Impl extends AbstractOpenemsModbusComponent
 		}
 		this._setCapacity(this.config.capacity());
 		this._setMaxApparentPower(this.config.maxApparentPower());
-		this._setAllowedChargePower(-Math.abs(this.config.maxChargePower()));
+		this.getAllowedChargePowerChannel().setNextValue(-Math.abs(this.config.maxChargePower()));
 		this._setAllowedDischargePower(Math.abs(this.config.maxDischargePower()));
 	}
 
