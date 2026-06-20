6822540a7edf1b8e6baf7af097a91d23f804ed3e
Doanh Nguyen <doanhnguyen2.com@gmail.com>
2026-06-20 22:50:25 +0700

    Fix SG110CX input register addressing

--- io.openems.edge.pvinverter.sungrow.sg110cx/src/io/openems/edge/pvinverter/sungrow/sg110cx/PvInverterSungrowSg110CxImpl.java
+++ io.openems.edge.pvinverter.sungrow.sg110cx/src/io/openems/edge/pvinverter/sungrow/sg110cx/PvInverterSungrowSg110CxImpl.java
@@ -33,7 +33,7 @@ import io.openems.edge.bridge.modbus.api.element.SignedDoublewordElement;
 import io.openems.edge.bridge.modbus.api.element.SignedWordElement;
 import io.openems.edge.bridge.modbus.api.element.UnsignedDoublewordElement;
 import io.openems.edge.bridge.modbus.api.element.UnsignedWordElement;
-import io.openems.edge.bridge.modbus.api.task.FC3ReadRegistersTask;
+import io.openems.edge.bridge.modbus.api.task.FC4ReadInputRegistersTask;
 import io.openems.edge.common.component.OpenemsComponent;
 import io.openems.edge.common.modbusslave.ModbusSlave;
 import io.openems.edge.common.modbusslave.ModbusSlaveTable;
@@ -96,114 +96,114 @@ public class PvInverterSungrowSg110CxImpl extends AbstractOpenemsModbusComponent
 	@Override
 	protected ModbusProtocol defineModbusProtocol() {
 		return new ModbusProtocol(this, //
-				new FC3ReadRegistersTask(5004, Priority.HIGH, //
+				new FC4ReadInputRegistersTask(5003, Priority.HIGH, //
 						m(ElectricityMeter.ChannelId.ACTIVE_PRODUCTION_ENERGY,
-								new UnsignedDoublewordElement(5004).wordOrder(LSWMSW), SCALE_FACTOR_3)), //
-				new FC3ReadRegistersTask(5008, Priority.HIGH, //
-						m(PvInverterSungrowSg110Cx.ChannelId.TMP_CAB, new SignedWordElement(5008),
+								new UnsignedDoublewordElement(5003).wordOrder(LSWMSW), SCALE_FACTOR_3)), //
+				new FC4ReadInputRegistersTask(5007, Priority.HIGH, //
+						m(PvInverterSungrowSg110Cx.ChannelId.TMP_CAB, new SignedWordElement(5007),
 								SCALE_FACTOR_MINUS_1)), //
-				new FC3ReadRegistersTask(5011, Priority.HIGH, //
-						m(new UnsignedWordElement(5011)) //
+				new FC4ReadInputRegistersTask(5010, Priority.HIGH, //
+						m(new UnsignedWordElement(5010)) //
 								.m(PvInverterSungrowSg110Cx.ChannelId.PV1_VOLTAGE, SCALE_FACTOR_2) //
 								.m(PvInverterSungrowSg110Cx.ChannelId.PV2_VOLTAGE, SCALE_FACTOR_2) //
 								.build(), //
-						new DummyRegisterElement(5012), //
-						m(new UnsignedWordElement(5013)) //
+						new DummyRegisterElement(5011), //
+						m(new UnsignedWordElement(5012)) //
 								.m(PvInverterSungrowSg110Cx.ChannelId.PV3_VOLTAGE, SCALE_FACTOR_2) //
 								.m(PvInverterSungrowSg110Cx.ChannelId.PV4_VOLTAGE, SCALE_FACTOR_2) //
 								.build(), //
-						new DummyRegisterElement(5014), //
-						m(new UnsignedWordElement(5015)) //
+						new DummyRegisterElement(5013), //
+						m(new UnsignedWordElement(5014)) //
 								.m(PvInverterSungrowSg110Cx.ChannelId.PV5_VOLTAGE, SCALE_FACTOR_2) //
 								.m(PvInverterSungrowSg110Cx.ChannelId.PV6_VOLTAGE, SCALE_FACTOR_2) //
 								.build()), //
-				new FC3ReadRegistersTask(5019, Priority.HIGH, //
-						m(PvInverterSungrowSg110Cx.ChannelId.VOLTAGE_L1_L2, new UnsignedWordElement(5019),
+				new FC4ReadInputRegistersTask(5018, Priority.HIGH, //
+						m(PvInverterSungrowSg110Cx.ChannelId.VOLTAGE_L1_L2, new UnsignedWordElement(5018),
 								SCALE_FACTOR_2), //
-						m(PvInverterSungrowSg110Cx.ChannelId.VOLTAGE_L2_L3, new UnsignedWordElement(5020),
+						m(PvInverterSungrowSg110Cx.ChannelId.VOLTAGE_L2_L3, new UnsignedWordElement(5019),
 								SCALE_FACTOR_2), //
-						m(PvInverterSungrowSg110Cx.ChannelId.VOLTAGE_L3_L1, new UnsignedWordElement(5021),
+						m(PvInverterSungrowSg110Cx.ChannelId.VOLTAGE_L3_L1, new UnsignedWordElement(5020),
 								SCALE_FACTOR_2), //
-						m(ElectricityMeter.ChannelId.CURRENT_L1, new UnsignedWordElement(5022), SCALE_FACTOR_2), //
-						m(ElectricityMeter.ChannelId.CURRENT_L2, new UnsignedWordElement(5023), SCALE_FACTOR_2), //
-						m(ElectricityMeter.ChannelId.CURRENT_L3, new UnsignedWordElement(5024), SCALE_FACTOR_2)), //
-				new FC3ReadRegistersTask(5031, Priority.HIGH, //
+						m(ElectricityMeter.ChannelId.CURRENT_L1, new UnsignedWordElement(5021), SCALE_FACTOR_2), //
+						m(ElectricityMeter.ChannelId.CURRENT_L2, new UnsignedWordElement(5022), SCALE_FACTOR_2), //
+						m(ElectricityMeter.ChannelId.CURRENT_L3, new UnsignedWordElement(5023), SCALE_FACTOR_2)), //
+				new FC4ReadInputRegistersTask(5030, Priority.HIGH, //
 						m(ElectricityMeter.ChannelId.ACTIVE_POWER,
-								new UnsignedDoublewordElement(5031).wordOrder(LSWMSW)), //
+								new UnsignedDoublewordElement(5030).wordOrder(LSWMSW)), //
 						m(ElectricityMeter.ChannelId.REACTIVE_POWER,
-								new SignedDoublewordElement(5033).wordOrder(LSWMSW)), //
-						m(PvInverterSungrowSg110Cx.ChannelId.POWER_FACTOR, new SignedWordElement(5035),
+								new SignedDoublewordElement(5032).wordOrder(LSWMSW)), //
+						m(PvInverterSungrowSg110Cx.ChannelId.POWER_FACTOR, new SignedWordElement(5034),
 								POWER_FACTOR_SCALE_FACTOR), //
-						m(ElectricityMeter.ChannelId.FREQUENCY, new UnsignedWordElement(5036), SCALE_FACTOR_2)), //
-				new FC3ReadRegistersTask(5038, Priority.HIGH, //
-						m(PvInverterSungrowSg110Cx.ChannelId.OPERATING_STATUS, new UnsignedWordElement(5038))), //
-				new FC3ReadRegistersTask(5115, Priority.HIGH, //
-						m(new UnsignedWordElement(5115)) //
+						m(ElectricityMeter.ChannelId.FREQUENCY, new UnsignedWordElement(5035), SCALE_FACTOR_2)), //
+				new FC4ReadInputRegistersTask(5037, Priority.HIGH, //
+						m(PvInverterSungrowSg110Cx.ChannelId.OPERATING_STATUS, new UnsignedWordElement(5037))), //
+				new FC4ReadInputRegistersTask(5114, Priority.HIGH, //
+						m(new UnsignedWordElement(5114)) //
 								.m(PvInverterSungrowSg110Cx.ChannelId.PV7_VOLTAGE, SCALE_FACTOR_2) //
 								.m(PvInverterSungrowSg110Cx.ChannelId.PV8_VOLTAGE, SCALE_FACTOR_2) //
 								.build(), //
-						new DummyRegisterElement(5116), //
-						m(new UnsignedWordElement(5117)) //
+						new DummyRegisterElement(5115), //
+						m(new UnsignedWordElement(5116)) //
 								.m(PvInverterSungrowSg110Cx.ChannelId.PV9_VOLTAGE, SCALE_FACTOR_2) //
 								.m(PvInverterSungrowSg110Cx.ChannelId.PV10_VOLTAGE, SCALE_FACTOR_2) //
 								.build(), //
-						new DummyRegisterElement(5118), //
-						m(new UnsignedWordElement(5119)) //
+						new DummyRegisterElement(5117), //
+						m(new UnsignedWordElement(5118)) //
 								.m(PvInverterSungrowSg110Cx.ChannelId.PV11_VOLTAGE, SCALE_FACTOR_2) //
 								.m(PvInverterSungrowSg110Cx.ChannelId.PV12_VOLTAGE, SCALE_FACTOR_2) //
 								.build(), //
-						new DummyRegisterElement(5120), //
-						m(new UnsignedWordElement(5121)) //
+						new DummyRegisterElement(5119), //
+						m(new UnsignedWordElement(5120)) //
 								.m(PvInverterSungrowSg110Cx.ChannelId.PV13_VOLTAGE, SCALE_FACTOR_2) //
 								.m(PvInverterSungrowSg110Cx.ChannelId.PV14_VOLTAGE, SCALE_FACTOR_2) //
 								.build(), //
-						new DummyRegisterElement(5122), //
-						m(new UnsignedWordElement(5123)) //
+						new DummyRegisterElement(5121), //
+						m(new UnsignedWordElement(5122)) //
 								.m(PvInverterSungrowSg110Cx.ChannelId.PV15_VOLTAGE, SCALE_FACTOR_2) //
 								.m(PvInverterSungrowSg110Cx.ChannelId.PV16_VOLTAGE, SCALE_FACTOR_2) //
 								.build()), //
-				new FC3ReadRegistersTask(5130, Priority.HIGH, //
-						m(new UnsignedWordElement(5130)) //
+				new FC4ReadInputRegistersTask(5129, Priority.HIGH, //
+						m(new UnsignedWordElement(5129)) //
 								.m(PvInverterSungrowSg110Cx.ChannelId.PV17_VOLTAGE, SCALE_FACTOR_2) //
 								.m(PvInverterSungrowSg110Cx.ChannelId.PV18_VOLTAGE, SCALE_FACTOR_2) //
 								.build()), //
-				new FC3ReadRegistersTask(7013, Priority.HIGH, //
-						m(PvInverterSungrowSg110Cx.ChannelId.PV1_CURRENT, new UnsignedWordElement(7013),
+				new FC4ReadInputRegistersTask(7012, Priority.HIGH, //
+						m(PvInverterSungrowSg110Cx.ChannelId.PV1_CURRENT, new UnsignedWordElement(7012),
 								SCALE_FACTOR_1), //
-						m(PvInverterSungrowSg110Cx.ChannelId.PV2_CURRENT, new UnsignedWordElement(7014),
+						m(PvInverterSungrowSg110Cx.ChannelId.PV2_CURRENT, new UnsignedWordElement(7013),
 								SCALE_FACTOR_1), //
-						m(PvInverterSungrowSg110Cx.ChannelId.PV3_CURRENT, new UnsignedWordElement(7015),
+						m(PvInverterSungrowSg110Cx.ChannelId.PV3_CURRENT, new UnsignedWordElement(7014),
 								SCALE_FACTOR_1), //
-						m(PvInverterSungrowSg110Cx.ChannelId.PV4_CURRENT, new UnsignedWordElement(7016),
+						m(PvInverterSungrowSg110Cx.ChannelId.PV4_CURRENT, new UnsignedWordElement(7015),
 								SCALE_FACTOR_1), //
-						m(PvInverterSungrowSg110Cx.ChannelId.PV5_CURRENT, new UnsignedWordElement(7017),
+						m(PvInverterSungrowSg110Cx.ChannelId.PV5_CURRENT, new UnsignedWordElement(7016),
 								SCALE_FACTOR_1), //
-						m(PvInverterSungrowSg110Cx.ChannelId.PV6_CURRENT, new UnsignedWordElement(7018),
+						m(PvInverterSungrowSg110Cx.ChannelId.PV6_CURRENT, new UnsignedWordElement(7017),
 								SCALE_FACTOR_1), //
-						m(PvInverterSungrowSg110Cx.ChannelId.PV7_CURRENT, new UnsignedWordElement(7019),
+						m(PvInverterSungrowSg110Cx.ChannelId.PV7_CURRENT, new UnsignedWordElement(7018),
 								SCALE_FACTOR_1), //
-						m(PvInverterSungrowSg110Cx.ChannelId.PV8_CURRENT, new UnsignedWordElement(7020),
+						m(PvInverterSungrowSg110Cx.ChannelId.PV8_CURRENT, new UnsignedWordElement(7019),
 								SCALE_FACTOR_1), //
-						m(PvInverterSungrowSg110Cx.ChannelId.PV9_CURRENT, new UnsignedWordElement(7021),
+						m(PvInverterSungrowSg110Cx.ChannelId.PV9_CURRENT, new UnsignedWordElement(7020),
 								SCALE_FACTOR_1), //
-						m(PvInverterSungrowSg110Cx.ChannelId.PV10_CURRENT, new UnsignedWordElement(7022),
+						m(PvInverterSungrowSg110Cx.ChannelId.PV10_CURRENT, new UnsignedWordElement(7021),
 								SCALE_FACTOR_1), //
-						m(PvInverterSungrowSg110Cx.ChannelId.PV11_CURRENT, new UnsignedWordElement(7023),
+						m(PvInverterSungrowSg110Cx.ChannelId.PV11_CURRENT, new UnsignedWordElement(7022),
 								SCALE_FACTOR_1), //
-						m(PvInverterSungrowSg110Cx.ChannelId.PV12_CURRENT, new UnsignedWordElement(7024),
+						m(PvInverterSungrowSg110Cx.ChannelId.PV12_CURRENT, new UnsignedWordElement(7023),
 								SCALE_FACTOR_1), //
-						m(PvInverterSungrowSg110Cx.ChannelId.PV13_CURRENT, new UnsignedWordElement(7025),
+						m(PvInverterSungrowSg110Cx.ChannelId.PV13_CURRENT, new UnsignedWordElement(7024),
 								SCALE_FACTOR_1), //
-						m(PvInverterSungrowSg110Cx.ChannelId.PV14_CURRENT, new UnsignedWordElement(7026),
+						m(PvInverterSungrowSg110Cx.ChannelId.PV14_CURRENT, new UnsignedWordElement(7025),
 								SCALE_FACTOR_1), //
-						m(PvInverterSungrowSg110Cx.ChannelId.PV15_CURRENT, new UnsignedWordElement(7027),
+						m(PvInverterSungrowSg110Cx.ChannelId.PV15_CURRENT, new UnsignedWordElement(7026),
 								SCALE_FACTOR_1), //
-						m(PvInverterSungrowSg110Cx.ChannelId.PV16_CURRENT, new UnsignedWordElement(7028),
+						m(PvInverterSungrowSg110Cx.ChannelId.PV16_CURRENT, new UnsignedWordElement(7027),
 								SCALE_FACTOR_1), //
-						m(PvInverterSungrowSg110Cx.ChannelId.PV17_CURRENT, new UnsignedWordElement(7029),
+						m(PvInverterSungrowSg110Cx.ChannelId.PV17_CURRENT, new UnsignedWordElement(7028),
 								SCALE_FACTOR_1)), //
-				new FC3ReadRegistersTask(7032, Priority.HIGH, //
-						m(PvInverterSungrowSg110Cx.ChannelId.PV18_CURRENT, new UnsignedWordElement(7032),
+				new FC4ReadInputRegistersTask(7031, Priority.HIGH, //
+						m(PvInverterSungrowSg110Cx.ChannelId.PV18_CURRENT, new UnsignedWordElement(7031),
 								SCALE_FACTOR_1)) //
 		);
 	}
