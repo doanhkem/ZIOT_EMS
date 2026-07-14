707202c5a37147e83041c728dd23c063b1cc8597
Doanh Nguyen <doanhnguyen2.com@gmail.com>
2026-07-14 11:18:14 +0700

    Add readable sell-to-grid control log

--- io.openems.edge.controller.pvinverter.selltogridlimit/src/io/openems/edge/controller/pvinverter/selltogridlimit/ControllerPvInverterSellToGridLimitImpl.java
+++ io.openems.edge.controller.pvinverter.selltogridlimit/src/io/openems/edge/controller/pvinverter/selltogridlimit/ControllerPvInverterSellToGridLimitImpl.java
@@ -1,5 +1,7 @@
 package io.openems.edge.controller.pvinverter.selltogridlimit;
 
+import java.util.Locale;
+
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -7,6 +9,8 @@ import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.metatype.annotations.Designate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.openems.common.exceptions.InvalidValueException;
 import io.openems.common.exceptions.OpenemsError.OpenemsNamedException;
@@ -27,6 +31,8 @@ public class ControllerPvInverterSellToGridLimitImpl extends AbstractOpenemsComp
 		implements ControllerPvInverterSellToGridLimit, Controller, OpenemsComponent {
 
 	public static final double DEFAULT_MAX_ADJUSTMENT_RATE = 0.2;
+
+	private final Logger log = LoggerFactory.getLogger(ControllerPvInverterSellToGridLimitImpl.class);
 
 	@Reference
 	private ComponentManager componentManager;
@@ -54,47 +60,16 @@ public class ControllerPvInverterSellToGridLimitImpl extends AbstractOpenemsComp
 		super.deactivate();
 	}
 
-	/**
-	 * Calculates required charge/discharge power.
-	 *
-	 * @param pvInverter     the SymmetricPvInverter
-	 * @param meter          the Meter
-	 * @param asymmetricMode is asymmetric mode configured
-	 * @return the required power
-	 * @throws InvalidValueException on error
-	 */
-	private int calculateRequiredPower(ManagedSymmetricPvInverter pvInverter, ElectricityMeter meter,
-			boolean asymmetricMode) throws InvalidValueException {
-
-		/*
-		 * Calculate grid-power
-		 */
-		var gridPower = 0;
-		var maximumSellToGridPower = this.config.maximumSellToGridPower();
-		if (asymmetricMode) {
-			// TODO: Optimize for Single-Phase PV-Inverter
-			var gridPowerL1 = meter.getActivePowerL1().getOrError();
-			var gridPowerL2 = meter.getActivePowerL2().getOrError();
-			var gridPowerL3 = meter.getActivePowerL3().getOrError();
-
-			var minPowerOnPhase = Math.min(Math.min(gridPowerL1, gridPowerL2), gridPowerL3);
-			gridPower = minPowerOnPhase * 3;
-			maximumSellToGridPower *= 3;
-		} else {
-			gridPower = meter.getActivePower().getOrError();
-		}
-		return gridPower /* current buy-from/sell-to grid */
-				+ pvInverter.getActivePower().getOrError() /* current production */
-				+ maximumSellToGridPower; /* the configured limit */
-	}
-
 	@Override
 	public void run() throws OpenemsNamedException {
 		ManagedSymmetricPvInverter pvInverter = this.componentManager.getComponent(this.config.pvInverter_id());
 		ElectricityMeter meter = this.componentManager.getComponent(this.config.meter_id());
 
 		// Calculates required charge/discharge power
-		var calculatedPower = this.calculateRequiredPower(pvInverter, meter, this.config.asymmetricMode());
+		var gridPower = this.getGridPower(meter, this.config.asymmetricMode());
+		var pvPower = pvInverter.getActivePower().getOrError();
+		var maximumSellToGridPower = this.getMaximumSellToGridPower(this.config.asymmetricMode());
+		var calculatedPower = gridPower + pvPower + maximumSellToGridPower;
 
 		if (Math.abs(this.lastSetLimit) > 100 && Math.abs(calculatedPower) > 100 && Math
 				.abs(this.lastSetLimit - calculatedPower) > Math.abs(this.lastSetLimit) * DEFAULT_MAX_ADJUSTMENT_RATE) {
@@ -109,5 +84,43 @@ public class ControllerPvInverterSellToGridLimitImpl extends AbstractOpenemsComp
 
 		// set result
 		pvInverter.setActivePowerLimit(calculatedPower);
+		this.logControlWrite(pvInverter, gridPower, pvPower, maximumSellToGridPower, calculatedPower);
+	}
+
+	private int getGridPower(ElectricityMeter meter, boolean asymmetricMode) throws InvalidValueException {
+		if (!asymmetricMode) {
+			return meter.getActivePower().getOrError();
+		}
+		var gridPowerL1 = meter.getActivePowerL1().getOrError();
+		var gridPowerL2 = meter.getActivePowerL2().getOrError();
+		var gridPowerL3 = meter.getActivePowerL3().getOrError();
+		return Math.min(Math.min(gridPowerL1, gridPowerL2), gridPowerL3) * 3;
+	}
+
+	private int getMaximumSellToGridPower(boolean asymmetricMode) {
+		var maximumSellToGridPower = this.config.maximumSellToGridPower();
+		return asymmetricMode ? maximumSellToGridPower * 3 : maximumSellToGridPower;
+	}
+
+	private void logControlWrite(ManagedSymmetricPvInverter pvInverter, int gridPower, int pvPower,
+			int maximumSellToGridPower, int calculatedPower) {
+		var writePercent = this.toPercent(pvInverter, calculatedPower);
+		this.log.info("CTRL_WRITE_OK ⚡ Grid={} kW | 🏭 Load={} kW | ☀ PV={} kW | 🎯 Limit={} kW | ✍ Write={}{} -> {}",
+				formatKw(gridPower), formatKw(gridPower + pvPower), formatKw(pvPower), formatKw(maximumSellToGridPower),
+				writePercent == null ? formatKw(calculatedPower) : writePercent, writePercent == null ? " kW" : "%",
+				this.config.pvInverter_id());
+	}
+
+	private String toPercent(ManagedSymmetricPvInverter pvInverter, int power) {
+		var maxPower = pvInverter.getMaxApparentPower().orElse(0);
+		if (maxPower <= 0) {
+			return null;
+		}
+		var percent = Math.max(-100, Math.min(100, Math.round(power * 100F / maxPower)));
+		return String.format(Locale.ROOT, "%.2f", (double) percent);
+	}
+
+	private static String formatKw(int watts) {
+		return String.format(Locale.ROOT, "%.2f", watts / 1000.0);
 	}
 }
